8324cbdd71f4966c02fb3d38f51e1f0bf67b12b5
Jiayin Li <lijiayin1983@gmail.com>
2016-09-01 17:29:59 -0400

    modified after running code on leetcode

--- 001_two_sum/main.cpp
+++ 001_two_sum/main.cpp
@@ -5,24 +5,31 @@ using namespace std;
 class Solution {
     public:
     	vector<int> twoSum(vector<int>& nums, int target) {
-    		vector <int> res;
+    		vector <int> res(2);
+    		bool found = false;
         	for (vector<int>::iterator i = nums.begin(); i != nums.end(); i++){
         		for (vector<int>::iterator j = i + 1; j != nums.end(); j++){
         			if (*i + *j == target){
-        				res.push_back(*i);
-        				res.push_back(*j);
-        				return res;
+        				res[0]=i-nums.begin();
+        				res[1]=j-nums.begin();
+        				found = true;
+        				break;
         			}
         		}
+        		if (found){
+        		    break;
+        		}
+        		
         	}
+        	return res;
         }
 };
 
 int main(){
 
-	int value[] = {1,2,3,4,5};
+	int value[] = {3,2,4};
 	vector<int> input(value, value + sizeof(value) / sizeof(int) );
-	int my_target = 9;
+	int my_target = 6;
 	Solution sol;
 	vector<int> res = sol.twoSum(input, my_target);
 	for (vector<int>::iterator i = res.begin(); i != res.end(); i++){
