a072594836e5621ca541ee9d66487ce953881eb1
Jiayin Li <lijiayin1983@gmail.com>
2016-09-01 17:08:04 -0400

    initiial check-in
    solution for #001 tow_sum

--- 001_two_sum/main.cpp
+++ 001_two_sum/main.cpp
@@ -1,6 +1,31 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
 class Solution {
     public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-                    
-                        }
+    	vector<int> twoSum(vector<int>& nums, int target) {
+    		vector <int> res;
+        	for (vector<int>::iterator i = nums.begin(); i != nums.end(); i++){
+        		for (vector<int>::iterator j = i + 1; j != nums.end(); j++){
+        			if (*i + *j == target){
+        				res.push_back(*i);
+        				res.push_back(*j);
+        				return res;
+        			}
+        		}
+        	}
+        }
 };
+
+int main(){
+
+	int value[] = {1,2,3,4,5};
+	vector<int> input(value, value + sizeof(value) / sizeof(int) );
+	int my_target = 9;
+	Solution sol;
+	vector<int> res = sol.twoSum(input, my_target);
+	for (vector<int>::iterator i = res.begin(); i != res.end(); i++){
+		cout<< *i <<endl;
+	}
+}
