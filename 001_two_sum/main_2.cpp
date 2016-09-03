e6fd1dc9948bcbff7b6ca8d64669ac9f087efa89
Jiayin Li <lijiayin1983@gmail.com>
2016-09-03 12:10:48 -0400

    modify 001 solution

--- 001_two_sum/main_2.cpp
+++ 001_two_sum/main_2.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 class Solution {
     public:
-    	vector<int> twoSum(vector<int>& nums, int target) {
-    		vector <int> res(2);
+        vector<int> twoSum(vector<int>& nums, int target) {
+            vector <int> res(2);
             map<int,int> value_map;
             int value_needed;
-    		for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++){
-    			value_needed = target - *it;
-    			map<int,int>::iterator j_it = value_map.find(value_needed);
-    			if (j_it != value_map.end()){
-    				//the other value is found
-    				res[0] = j_it->second;
-    				res[1] = it - nums.begin();
-    				return res;
-    			}
-    			map<int,int>::iterator search_it = value_map.find(*it);
-    			if (j_it == value_map.end()){
-    				//insert value to hash table
-    				value_map[*it] = it - nums.begin();
-    			}
-
-    		}
-        }
+            for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++){
+                value_needed = target - *it;
+                map<int,int>::iterator j_it = value_map.find(value_needed);
+                if (j_it != value_map.end()){
+                    //the other value is found
+                    res[0] = j_it->second;
+                    res[1] = it - nums.begin();
+                    break;
+                }
+                map<int,int>::iterator search_it = value_map.find(*it);
+                if (j_it == value_map.end()){
+                    //insert value to hash table
+                    value_map[*it] = it - nums.begin();
+                }
+            }
+            return res;
+    }
 };
 
 int main(){
