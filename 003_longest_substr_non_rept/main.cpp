017a54e5de1eeb7715c6e476e34f02d1608c78ce
Jiayin Li <lijiayin1983@gmail.com>
2016-09-14 16:07:53 -0400

    add solution for 003

---
+++ 003_longest_substr_non_rept/main.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    	int s_len = s.length();
+    	int l_index = 0;
+    	int r_index = 0;
+    	int max_len = 0;
+    	int tmp_len;
+
+		unordered_map<char,int> hash;
+    	if (s_len <= 1){
+    		return s_len;
+    	}
+    	
+    	r_index = 0; 
+    	
+    	while (r_index < s_len){
+
+    		if (hash.find(s[r_index]) == hash.end() or hash[s[r_index]] == 0){
+    			hash[s[r_index]] = 1;
+    			tmp_len = r_index - l_index + 1;
+    		}
+    		else {
+    			while(s[r_index] != s[l_index]){
+    			    hash[s[l_index]] = 0;
+    				l_index++;
+    			}
+    			//hash[s[r_index]] = 0;
+    			l_index++;
+    			tmp_len = r_index - l_index + 1;
+    		}
+    		if (tmp_len >= max_len) {
+    			max_len = tmp_len;
+    		}
+    		r_index++;
+    	}
+    	return max_len;
+    }
+};
